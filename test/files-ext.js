f24b939ccc9c3cf591b579f9804f50a2d520011e
Luis Vega <luiscvega@gmail.com>
2014-04-14 00:49:47 +0800

    Just pass in path

--- test/files-ext.js
+++ test/files-ext.js
@@ -3,11 +3,7 @@ var fe = require("..");
 
 test("list all non-index js files", function (t) {
   var files = fe(__dirname + "/files").sort()
-    , expected = [
-        "one",
-        "three",
-        "two"
-      ];
+    , expected = ["one", "three", "two"];
 
   t.deepEqual(files, expected);
   t.end();
